797351cfeb91fb2f61c88b869795516841b95a45
Milad Ramezani <milad@robots.ox.ac.uk>
2020-06-01 15:51:22 +0100

    alpha sym added to 3d slam

--- vertigo/include/fullSLAM.h
+++ vertigo/include/fullSLAM.h
@@ -39,6 +39,9 @@ namespace fullSLAM {
   /// Convenience function for constructing a pose key
   inline Symbol PointKey(Eigen::Index j) { return Symbol('l', j); }
 
+  /// Shape param
+  inline Symbol AlphaKey() { return Symbol('a'); }
+
   /*
    * List of typedefs for factors
    */
